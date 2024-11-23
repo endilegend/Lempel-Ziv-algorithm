3cb7d0873c9fb41a83a0e56e71bd9e30c466f4f0
Endi <troqeendi@gmail.com>
2024-11-23 18:03:10 -0500

    finished the LZ algorithm.

--- lzalgorithm.py
+++ lzalgorithm.py
@@ -1,36 +1,51 @@
-def lzAlgorithim(value):
+def uniqueSubstrings(value):
+    
     numbersRepresentation = {}
 
     num = 1
-    print(value)
-    for i,x in enumerate(value):
-        if x not in numbersRepresentation.values():
-            numbersRepresentation[num] = x
+    i = 0
+    while i < len(value):
+        if value[i] not in numbersRepresentation.values():
+            numbersRepresentation[num] = value[i]
+            z = i
+
         else:
             z = i+1
             while z < len(value):
-                if value[i:z+1] not in numbersRepresentation.values():
-                    numbersRepresentation[num] = value[i:z+1]
+                rep = value[i:z+1]
+                if rep not in numbersRepresentation.values():
+                    numbersRepresentation[num]= rep
                     break
-                z +=1
-        num += 1
-
-    outliers = "".join([value for value in numbersRepresentation.values()])
-    if len(outliers) < len(value):
-        remaining = len(outliers) - len(value)
-        outliers += value[remaining:]
-
-
-    
-    
-
+                z += 1
+        num+=1
+        i = z +1 
     return numbersRepresentation
-                
 
-
-
+def encode(uniqueSubstring):
     
+    values = list(uniqueSubstring.values())
+    encoded = {1: "1", 2: "0"}
+    high = 0
 
+    for i in range(3,len(values)+1):
+        x = (uniqueSubstring[i])[:-1]
+        if x in values: 
+            encoded[i] = f"{values.index(x)+1}{uniqueSubstring[i][-1:]}" 
+            high = max(high, values.index(x)+1)
+
+    # got the .bit_length() from stack overflow
+    high = high.bit_length()
+
+    for i in range(3,len(values)+1):
+        x = int((encoded[i])[:-1])
+        #got how to get the minimum bits from ChatGPT
+        x = f"{x:0{high}b}"
+
+        encoded[i]= f"{x}{encoded[i][-1:]}"
+
+    encoded = "".join(list(encoded.values()))
+    
+    return encoded     
 
 
 from PIL import Image
@@ -56,6 +71,6 @@ for i in range(image.size[0]):
         values[i,j] = "".join(p)
 
 for key,value in values.items():
-    (lzAlgorithim(value))
-
+    uniqueSubstring = (uniqueSubstrings(value))
+    print(encode(uniqueSubstring))
 
